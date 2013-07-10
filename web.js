4431f9749ce1d3f3df4da9cd4c996552c07bcf9d
Ubuntu <ubuntu@ip-172-31-27-200.us-west-2.compute.internal>
2013-07-10 21:44:24 +0000

    fix

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-var txt = fs.readFile("index.html");
+var txt = new Buffer(fs.readFile("index.html"));
 app.get('/', function(request, response) {
   response.send(txt.toString());
 });

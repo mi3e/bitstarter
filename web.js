7e65e5109757ef45f84a7ead9ec2c435f353dd1c
Ubuntu <ubuntu@ip-172-31-27-200.us-west-2.compute.internal>
2013-07-10 21:28:18 +0000

    Modifying web.js to read file from disk

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var txt = fs.readFile("index.html");
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(txt.toString());
 });
 
 var port = process.env.PORT || 5000;
